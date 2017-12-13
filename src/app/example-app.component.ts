c202712e199d5a65f9f9fb117560d812465a0416
Name <duengjen@students.zhaw.ch>
2017-12-13 10:11:47 +0100

    updated

--- src/app/example-app.component.ts
+++ src/app/example-app.component.ts
@@ -43,7 +43,7 @@ export class ExampleAppComponent extends path.PathAppComponent {
 
     public getBackendUrl() {
         if (window.location.hostname.indexOf("herokuapp") != -1) {
-            return location.protocol + "//jedue/services";
+            return location.protocol + "//jedue.herokuapp.com/services";
         }
         return "http://localhost:8080/services";
     }
